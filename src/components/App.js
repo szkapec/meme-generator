06be92db72fcaf1e93750b6502c6675630f3a02b
szkapec <szkapec999@o2.pl>
2020-10-19 22:20:09 +0200

    add

--- src/components/App.js
+++ src/components/App.js
@@ -7,49 +7,48 @@ function App() {
   const [selectedId, setSelectedId] = useState(null);
   const [meme, setMeme] = useState(null)
 
-  // useEffect(() => {
-  //   fetch('https://api.imgflip.com/get_memes')
-  //     .then(res => res.json())
-  //     .then(res => setTemplates(res.data.memes))
-  // }, []);
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(res => res.json())
+      .then(res => setTemplates(res.data.memes))
+  }, []);
 
 
 
-  // const handleChange = (e) => {
-  //   const fildName = e.target.name;
-  //   const fieldValue = e.target.value;
-  //   if(fildName === "top" ) {
-  //     setTopText(fieldValue)
-  //   } else {
-  //     setBottomText(fieldValue)
-  //   }
-  // }
+  const handleChange = (e) => {
+    const fildName = e.target.name;
+    const fieldValue = e.target.value;
+    if(fildName === "top" ) {
+      setTopText(fieldValue)
+    } else {
+      setBottomText(fieldValue)
+    }
+  }
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const params = {
-  //     template_id: selectedId,
-  //     text0: toptext,
-  //     text1: bottomText,
-  //     username: 'xzk03017',
-  //     password: 'xzk03017@cndps.com'
-  //   }
-  //   fetch(`https://api.imgflip.com/caption_image?${qs.stringify(params)}`)
-  //     .then(resp => resp.json()
-  //     .then(json=> {
-  //       setMeme(json.data.url)
-  //     }))
-  //     setTopText('')
-  //     setBottomText('')
-  // }
-  // const handleClick = (imageId) => {
-  //   setSelectedId(imageId)
-  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const params = {
+      template_id: selectedId,
+      text0: toptext,
+      text1: bottomText,
+      username: 'xzk03017',
+      password: 'xzk03017@cndps.com'
+    }
+    fetch(`https://api.imgflip.com/caption_image?${qs.stringify(params)}`)
+      .then(resp => resp.json()
+      .then(json=> {
+        setMeme(json.data.url)
+      }))
+      setTopText('')
+      setBottomText('')
+  }
+  const handleClick = (imageId) => {
+    setSelectedId(imageId)
+  }
   console.log(templates)
   return (
     <div className="App">
-      assss
-        {/* <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className="container">
            <div> Wpisz tekst na góre obrazka</div>
           <input type="text" name="top" className="top" value={toptext} onChange={handleChange}/>
@@ -69,7 +68,7 @@ function App() {
             {templates && templates.map(item => (
               <img src={item.url} id={item.id} alt="item" onClick={() => handleClick(item.id)}/>
             ))}
-        </div> */}
+        </div>
     </div>
   );
 }
